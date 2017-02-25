3062b4024f7517e8f4a348bb922e79b4c9035f78
Nicolas Perraut <nicolas@ownweb.fr>
2017-02-25 22:37:56 +0100

    Comment copyright

--- copyright.js
+++ copyright.js
@@ -1,3 +1,6 @@
+/**
+ * Automatically set the copyright year  
+ */
 var year = new Date().getFullYear();
 var copyright = '&copy; ' + year + ' Oma Radio. Tous droits réservés.';
 document.getElementById('copyright_copyright').innerHTML = copyright;
